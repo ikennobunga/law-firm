b1c056db2d865633887f7ebf65db8a1bdead91a0
ikennobunga <ikennobunga@users.noreply.github.com>
2018-04-30 17:02:41 +0300

    deleted and redesigned the front page

--- pages/index.vue
+++ pages/index.vue
@@ -1,135 +1,43 @@
 <template>
-  <v-app class="intro">
-    <div class="loot">
-      <div class="plot1" elevation-20>
-        <Home
-          image="https://images.pexels.com/photos/404986/pexels-photo-404986.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-          height="817px"
-          title="the coon"
-          previewText="Awesome place"/>
+  <v-app>
+    <div class="hero">
+      <div class="heroImg">
+        <home
+          image="https://images.unsplash.com/photo-1515467068080-b58fdd1a9a66?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f2424faf1c14633e209d99d8a631c992&auto=format&fit=crop&w=900&q=60"
+          />
       </div>
-
-      <div class="plot2">
-        <h4>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim nobis libero, aspernatur ratione aliquam nisi cumque alias odit sint ducimus, nulla totam ut praesentium repellat pariatur dignissimos in laborum delectus.</h4>
-        <div class="buttons">
-          <div class="home">Home</div>
-          <div class="about">About</div>
-          <div class="practise">Practise</div>
-          <div class="contact">Contact</div>
-        </div>
-      </div>
+    </div>
+    <div class="sidebar">
+        <h1>side nav</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, ex quam expedita totam qui impedit exercitationem magnam necessitatibus repudiandae nostrum? Sequi voluptate mollitia dicta ipsum doloremque in laboriosam sed. Quaerat.</p>
     </div>
 
     <div class="article">
-      <div class="content1">
-        <h2><span class="bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam culpa quasi</span> consequuntur optio magni vel fugiat ipsum numquam ipsa velit iure aspernatur aut, explicabo nemo maiores distinctio, quae laborum eveniet.Eveniet harum placeat sint error sapiente molestiae! Voluptatibus animi fugit voluptatem iure eos similique soluta tempora quam reiciendis. Asperiores, incidunt quo repellendus possimus sed et totam similique iure unde corrupti?</h2>
+      <div class="articleText1">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis aut dolorum numquam, eius quam soluta ad ab explicabo totam ipsa ullam iure, doloribus amet laboriosam facilis ducimus quod? Nulla, porro.</p>
       </div>
+      <div class="articleText2">
+        <h1>What We Do.</h1>
+      </div>
+      <div class="cards">
 
-      <div class="contentText">
-        <h1>What We Do</h1>
       </div>
+      <div class="articleText3">
+        <h1>Clients Say.</h1>
+      </div>
+      <div class="card2">
 
-      <div class="content2 elevation-12">
-        <div class="contentPic1">
-          <v-flex xs12>
-            <div class="ourteam">
-              <img src="https://images.unsplash.com/photo-1497393007523-302c86e07c5d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f486a568fd0f7359e58d2d7f98232c7f&auto=format&fit=crop&w=900&q=60" alt="">
-               <div class="team-content">
-                <h3 class="title">Summer</h3>
-                <span class="post">Vaccay!!</span>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis soluta reprehenderit cumque minima earum voluptatibus dolores nihil numquam provident voluptatem. Sapiente blanditiis commodi veritatis quos, magnam excepturi accusantium praesentium et.</p>
-                <ul class="social">
-                  <li><a href="" class=""></a></li>
-                  <li><a href="" class=""></a></li>
-                  <li><a href="" class=""></a></li>
-                  <li><a href="" class=""></a></li>
-                </ul>
-              </div>
-            </div>
-          </v-flex>
-        </div>
-        <div class="contentPic2">
-          <Pic ourTeam="https://images.unsplash.com/photo-1504548214873-4bae9e99a24f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bcf33f0ac7e48a181a2260403489bd28&auto=format&fit=crop&w=900&q=60"/>
-        </div>
-        <div class="contentPic3">
-          <pic ourTeam="https://images.unsplash.com/photo-1505392049999-fb0564e097c2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1cb978a0935e6c70d2f4e449975c5699&auto=format&fit=crop&w=900&q=60"/>
-        </div>
       </div>
-    </div>
+      <div class="summery">
+        <div class="summeryText">
+          <h1>What to Expect</h1>
+        </div>
+        <div class="summeryImage">
 
-    <div class="article2">
-      <div class="contentText2">
-        <h1>Clients Say</h1>
+        </div>
       </div>
-
-      <div class="contentCards elevation-12">
-        <div class="card1">
-          <card
-           image1="https://images.unsplash.com/photo-1505845297973-ae7a996087c4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=29279d22b397388577cb213f474f0463&auto=format&fit=crop&w=900&q=60"
-           title="Summer Time"
-           textPreview="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ipsam saepe facilis, itaque ullam nulla repudiandae rerum eligendi repellendus officiis autem similique id in cum porro eaque earum tempore molestias."/>
-         </div>
-         <div class="card2">
-           <card
-            image1="https://images.unsplash.com/photo-1496747611176-843222e1e57c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=af153b6332902d06f7d573ef33f70d91&auto=format&fit=crop&w=900&q=60"
-            title="Sun and Beach"
-            textPreview="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ipsam saepe facilis, itaque ullam nulla repudiandae rerum eligendi repellendus officiis autem similique id in cum porro eaque earum tempore molestias."/>
-         </div>
-         <div class="card3">
-           <card
-            image1="https://images.unsplash.com/photo-1514179974491-a7885781ed87?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=733ff333379d2189c42e9a444d3884fa&auto=format&fit=crop&w=900&q=60"
-            title="Shopping"
-            textPreview="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ipsam saepe facilis, itaque ullam nulla repudiandae rerum eligendi repellendus officiis autem similique id in cum porro eaque earum tempore molestias."/>
-         </div>
-      </div>
-      <div class="contentText3">
-        <p class="contentPara1">
-          <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</span> Aliquam veniam iure asperiores quos? Velit consequuntur dolore distinctio numquam debitis aut quam ad, quia ex laudantium unde non delectus assumenda similique.
-          Sed, voluptas, beatae cumque laboriosam vero ipsum nobis qui repellat quos consectetur, veniam laudantium neque nam quasi saepe dignissimos dolore quisquam modi temporibus impedit itaque veritatis nemo doloremque! Quos, impedit!
-          Vero pariatur debitis commodi repellendus voluptatibus ratione. Quia fugiat, iure dignissimos sunt deleniti expedita! Reprehenderit magni odit et dolore libero beatae veniam culpa hic. Sunt exercitationem quod voluptatum magnam omnis.
-        </p>
-      </div>
-    </div>
-
-    <div class="article3">
-      <div class="content3">
-        <div class="contentText4">
-          <h1>What To Expect</h1>
-        </div>
-        <div class="article3pic">
-          <home class="elevation-22"
-           image="https://images.unsplash.com/photo-1523612192437-66de9804ac3c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=59310f54610ead00fcc3d07732d5d473&auto=format&fit=crop&w=900&q=60"/>
-       </div>
-      </div>
-
-      <div class="content4">
-        <div class="cardPlain1">
-          <card2
-           title="Commitment"
-           previewText="Cupcake ipsum dolor sit amet. Tart jujubes.Cupcake ipsum dolor sit amet. Tart jujubes.Cupcake ipsum dolor sit amet. Tart jujubes.Cupcake ipsum dolor sit amet. Tart jujubes.Cupcake ipsum dolor sit amet. Tart jujubesCupcake ipsum dolor sit amet."
-           btnText="More"/>
-        </div>
-        <div class="cardPlain2">
-          <card2
-           title="Trust"
-           previewText="Cupcake ipsum dolor sit amet. Tart jujubes.Cupcake ipsum dolor sit amet. Tart jujubes.Cupcake ipsum dolor sit amet. Tart jujubes.Cupcake ipsum dolor sit amet. Tart jujubes.Cupcake ipsum dolor sit amet. Tart jujubesCupcake ipsum dolor sit amet."
-           btnText="More"
-           />
-        </div>
-        <div class="cardPlain3">
-          <card2
-           title="Details"
-           previewText="Cupcake ipsum dolor sit amet. Tart jujubes.Cupcake ipsum dolor sit amet. Tart jujubes.Cupcake ipsum dolor sit amet. Tart jujubes.Cupcake ipsum dolor sit amet. Tart jujubes.Cupcake ipsum dolor sit amet. Tart jujubesCupcake ipsum dolor sit amet."
-           btnText="More"
-           />
-        </div>
-        <div class="cardPlain4">
-          <card2
-           title="Friendship"
-           previewText="Cupcake ipsum dolor sit amet. Tart jujubes.Cupcake ipsum dolor sit amet. Tart jujubes.Cupcake ipsum dolor sit amet. Tart jujubes.Cupcake ipsum dolor sit amet. Tart jujubes.Cupcake ipsum dolor sit amet. Tart jujubesCupcake ipsum dolor sit amet."
-           btnText="More"
-           />
-        </div>
+      <div class="footer">
+        <h1>footer</h1>
       </div>
     </div>
   </v-app>
@@ -137,284 +45,94 @@
 
 <script>
 import Home from '@/components/Home'
-import Card from '@/components/Card'
-import Card2 from '@/components/Card2'
 export default {
   components: {
-    Home,
-    Card,
-    Card2
+    Home
   }
 }
 </script>
 
 
 <style scoped>
-/*--------------------HEADER--------------------*/
-.intro{
-  background-color: #343233;
-
-}
-.loot{
+.hero{
   display: grid;
-  grid-template-columns: repeat(12, 1fr);
-  grid-template-rows: repeat(10, 1fr);
-  background-color: #343233;
-  height: 820px;
-  position: fixed;
+  grid-template-columns: 25vw auto;
+  background-color: bisque;
+  height: 800px;
 }
-.plot1{
-  grid-column: 1 / span 9;
-  grid-row: 1 / span 10; 
-}
-.plot2{
-  grid-column: 10 / span 12;
-  grid-row: 1 / span 10;
-  padding: 40px;
-  color: #E9DAB9;
-  width: 400px;
-  height: 817px;
-}
-.buttons{
+.sidebar{
   display: grid;
-  grid-template-columns: repeat(10, 1fr);
+  grid-template-columns: repeat(6, 1fr);
   grid-template-rows: repeat(12, 1fr);
-  grid-column-gap: 5px;
-  grid-row-gap: 5px;
-  height: 665px;
-  margin-bottom: 50px; 
+  position: fixed;
+  width: 25vw;
+  background-color: #FAFAFA;
+  height: auto;
 }
-.home{
-  grid-column: 1 / span 3;
-  grid-row: 11 / span 1;
-  display: flex;
-  justify-content: center;
-  align-items: center; 
-  border: 1px solid #4D4B4D;
+.heroImg{
+  grid-column-start: 2;
 }
-.about{
-  grid-column: 4 / span 6;
-  grid-row: 11 / span 1;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid #DAAF52;
-}
-.practise{
-  grid-column: 1 / span 4;
-  grid-row: 12 / span 1;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid #DAAF52;
-}
-.contact{
-  grid-column: 5 / span 5;
-  grid-row: 12 / span 1;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid #4D4B4D;
-}
-.plot1 .image{
-  width: 100%;
-  height: 810px;
+.image{
   background-size: cover;
-  color: aliceblue;
+  height: 784px;
 }
-/*--------------------ARTICLE--------------------*/
+/*----------ARTICLE----------*/
 .article{
   display: grid;
   grid-template-columns: repeat(12, 1fr);
-  grid-template-rows: repeat(20, 1fr);
-  background-color: #343233;  
-  height: 1000px;
-  position: relative;
-  margin-top: 820px;
+  grid-template-rows: repeat(54, 1fr);
+  height: 3500px;
+  background-color: #FFFFFF;
 }
-.content1{
-  grid-column: 3 / span 8;
-  grid-row: 1 / span 4;
-  /*border: 1px solid #4D4B4D;*/
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 0px;
+.articleText1{
+  grid-column: 6 / span 5;
+  grid-row: 3 / span 2;
+  background-color: aquamarine;
   text-align: center;
-  color: #E9DAB9;
 }
-.contentText{
-  grid-column: 1 / span 6;
-  grid-row: 7 / span 3;
-  font-family: 'Candal', sans-serif;
-  font-size: 50px;
-  color: #4D4B4D;
+.articleText2{
+  grid-column: 4 / span 5;
+  grid-row: 7 / span 2;
+  background-color: #F1948F;
 }
-.content2{
-  grid-column: 3 / span 10;
-  grid-row: 10 / span 8;
-  border: 20px solid #4D4B4D;
-  overflow: hidden;
+.cards{
+  grid-column: 7 / span 8;
+  grid-row: 9 / span 10;
+  border: 15px solid #4A5271;
+}
+.articleText3{
+  grid-column: 8 / span 4;
+  grid-row: 22 / span 2;
+  background-color: #39465B;
+}
+.card2{
+  grid-column: 4 / span 6;
+  grid-row: 24 / span 8;
+  border: 15px solid #4A5271;
+}
+.summery{
+  grid-column: 4 / span 9;
+  grid-row: 35 / span 10;
   display: grid;
   grid-template-columns: repeat(12, 1fr);
   grid-template-rows: repeat(12, 1fr);
-  height: 480px;
+  background-color: #907F78;
 }
-.bold{
-  font-size: 30px;
-}
-.ourteam{
-  position: relative;
-  overflow: hidden;
-  background-size: cover;
-}
-.ourteam img{
-  width: 100%;
-  height: 440px;
-  position: relative;
-  right: 0;
-  transition: all 0.4s ease-in-out 0s;
-  background-size: cover;
-}
-.ourteam:hover img{
-  right: 80%;
-}
-.ourteam .team-content{
-  width: 80%;
-  height: 100%;
-  padding: 20% 30px 0;
-  position: absolute;
-  top: 0;
-  right: -80%;
-  transition: all 0.4s ease-in-out 0s;
-  background-color: #86BADC;
-}
-.ourteam:hover .team-content{
-  right: 0;
-}
-.contentPic1{
-  grid-column: 1 / span 4;
-  grid-row: 1 / span 13;
-  background-color: #E9DAB9;
-}
-.contentPic2{
-  grid-column: 5 / span 4;
-  grid-row: 1 / span 12; 
-  background-color: #DAAF52;
-}
-.contentPic3{
-  grid-column: 9 / span 4;
-  grid-row: 1 / span 12;
-  background-color: aqua;
-}
-/*--------------------ARTICLE2--------------------*/
-.article2{
-  display: grid;
-  grid-template-columns: repeat(12, 1fr);
-  grid-template-rows: repeat(20, 1fr);
-  background-color: #343233;
-  height: 1000px;
-  position: relative;
-}
-.contentText2{
-  grid-column: 7 / span 6;
+.summeryText{
+  grid-column: 1 / span 9;
   grid-row: 1 / span 3;
-  font-family: 'Candal', sans-serif;
-  font-size: 50px;
-  color: #4D4B4D;
+  background-color: #4A5271;
 }
-.contentCards{
-  grid-column: 1 / span 10;
-  grid-row: 4 / span 9;
-  border: 20px solid #4D4B4D;
-  display: grid;
-  grid-template-columns: repeat(12, 1fr);
-  grid-template-rows: repeat(12,1fr);
-  grid-gap: 1px;
-}
-.card1{
-  grid-column: 1 / span 4;
-  grid-row: 1 / span 12;
-  background-color: antiquewhite;
-}
-.card2{
-  grid-column: 5 / span 4;
-  grid-row: 1 / span 12;
-  background-color: #39465B;
-}
-.card3{
-  grid-column: 9 / span 4;
-  grid-row: 1 / span 12;
-  background-color: #86BADC;
-}
-.contentText3{
-  grid-column: 3 / span 6;
-  grid-row: 15 / span 3;
-  color: #E9DAB9;
-  text-align: center;
-  font-size: 17px;
-}
-/*--------------------ARTICLE3--------------------*/
-.article3{
-  display: grid;
-  grid-template-columns: repeat(12, 1fr);
-  grid-template-rows: repeat(20, 1fr);
-  background-color: #343233;
-  height: 1000px;
-  position: relative;
-}
-.content3{
-  grid-column: 1 / span 12;
-  grid-row: 1 / span 20; 
-  background-color: #EDEAE3;
-  display: grid;
-  grid-template-columns: repeat(12, 1fr);
-  grid-template-rows: repeat(14, 1fr);
-}
-.contentText4{
-  grid-column: 3 / span 8;
-  grid-row: 1 / span 2;
-  font-family: 'Candal', sans-serif;
-  font-size: 50px;
-  color: rgba(233, 218, 185, 0.966);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-.article3pic{
+.summeryImage{
   grid-column: 2 / span 12;
-  grid-row: 3 / span 7;
-}
-.article3pic .image{
-  height: 100%;
-  background-size: cover;
-}
-.content4{
-  grid-column: 2 / span 12;
-  grid-row: 16 / span 5;
-  display: grid;
-  grid-template-columns: repeat(12, 1fr);
-  grid-template-rows: repeat(8, 1fr);
-  grid-column-gap: 5px;
-}
-.cardPlain1{
-  grid-column: 1 / span 3;
-  grid-row: 1 / span 8;
-  background-color: #4D4B4D
-}
-.cardPlain2{
-  grid-column: 4 / span 3;
-  grid-row: 1 / span 8;
+  grid-row: 4 / span 7; 
   background-color: #F1948F;
 }
-.cardPlain3{
-  grid-column: 7 / span 3;
-  grid-row: 1 / span 8;
-  background-color: #39465B;
-}
-.cardPlain4{
-  grid-column: 10 / span 3;
-  grid-row: 1 / span 8;
+/*----------FOOTER----------*/
+.footer{
+  grid-column: 4 / span 9;
+  grid-row: 47 / span 8;
+  background-color: aquamarine;
 }
 </style>
 
