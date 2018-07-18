9fa3a4e6c24f34ceb1ce534c513ce62c264a73e8
ikennobunga <ikennobunga@users.noreply.github.com>
2018-07-18 21:20:31 +0300

    created a new home page with stick pages on scroll

--- pages/index.vue
+++ pages/index.vue
@@ -2,65 +2,18 @@
   <v-app>
     
     <div class="hero">
-      <div class="heroPic">
-        <home image="/anders-jilden-239-unsplash.jpg"/>
-      </div>
+      <div class="heroPic"><h1>the pic</h1></div>
     </div>
 
-    <div class="article">
-      <div class="articleText1">
-        <p><span class="bold">What We Do.</span> </p>
-        <p>adipisicing elit. Quis aut dolorum numquam,eius quam soluta ad ab explicabo totam ipsa ullam iure, doloribus amet laboriosam facilis ducimus quod? Nulla, porro.</p>
-      </div>
-
-      <div class="article-img">
-        <img src="/rick-tap-110126.jpg" style="width:100%; height:100%;">
-      </div>
-      <div class="article-text">
-        <h1>Corporate & Commercial</h1>
-        <div id="line"></div>
-        <p class="article-paragraph">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis dolorem at aliquam ut magnam veritatis? Enim deleniti, ipsum dolorem culpa impedit blanditiis obcaecati exercitationem adipisci eveniet animi nisi neque earum!
-        </p>
-      </div>
-
-      <div class="article-img2">
-        <img src="/matt-artz-333274.jpg" style="width:100%; height:100%" alt="">
-      </div>
-      <div class="article-text2">
-        <h1>Regulatory & Complience</h1>
-        <div id="line"></div>
-        <p class="article-paragraph">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi illo expedita officia cum rem debitis saepe doloremque inventore, amet nemo dignissimos quas facere nostrum facilis aut libero suscipit deleniti necessitatibus?
-        </p>
-      </div>
-
-      <div class="article-img3">
-        <img src="/coins-1523383_1920.jpg" style="width:100%; height:100%;" alt="">
-      </div>
-      <div class="article-text3">
-        <h1>Financial Monetary Crime</h1>
-        <div id="line"></div>
-        <p class="article-paragraph">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam, commodi reprehenderit laborum ipsam dolor in accusamus. Fugiat soluta, excepturi enim a delectus nisi laboriosam pariatur architecto accusamus. Nesciunt, at quos!
-        </p>
-      </div>
-
-      <div class="article-img4">
-        <img src="/pacific-austin-106854.jpg" style="width:100%; height:100%;" alt="">
-      </div>
-      <div class="article-text4">
-        <h1>Mergers  and  Aquisitions</h1>
-        <div id="line"></div>
-        <p class="article-paragraph">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quod voluptatem pariatur eaque, dignissimos et accusamus, doloribus in perferendis reiciendis adipisci nisi rem soluta laudantium? Corrupti quasi saepe quam nesciunt?
-        </p>
-      </div>
-      
-      <div class="footer">
-        <h1>the footer</h1>
-      </div>
-    </div>
+    <section class="article">
+      <h1>WHO WE ARE</h1>
+    </section>
+    <section class="article2">
+      <h1>WHAT WE DO</h1>
+    </section>
+    <section class="article3">
+      <h1>WHAT OUR CLIENTS SAY</h1>
+    </section>
     
   </v-app>
 </template>
@@ -100,128 +53,42 @@ export default {
 
 <style scoped>
 .hero{
-  display: grid;
-  grid-template-columns: repeat(12, 1fr);
-  grid-template-rows: repeat(12, 1fr);
-  /*background-color: #91949D;*/
+  background-color: #91949D;
   width: 100%;
-  height: 840px;
-  position: absolute;
+  height: 100vh;
+  position: relative;
   margin-left: 360px;
   margin-top: -16px;
 }
 .heroPic{
-  grid-column: 1 / span 9;
-  grid-row: 1 / span 12;
-}
-.image{
-  height: 100%;
+  background-image: url('/anders-jilden-239-unsplash.jpg');
+  background-repeat: no-repeat;
+  background-size: contain;
   width: 100%;
-  background-size: cover;
+  height: 100%;
+  
 }
 /*----------ARTICLE----------*/
+.article, .article2, .article3{
+  position: sticky;
+  top: 0;
+}
 .article{
   display: grid;
-  grid-template-columns: repeat(12, 1fr);
-  grid-template-rows: repeat(54, 1fr);
-  height: 3800px;
-  margin-top: 800px;
-  background-color: #FFFFFF;
+  grid-template-columns: repeat();
+  grid-template-rows: repeat();
+  height: 100vh;
+  margin-left: 360px;
+  background-color: grey;
 }
-.articleText1{
-  grid-column: 5 / span 7;
-  grid-row: 3 / span 2;
-  text-align: center;
-  font-size: 18px;
-  color:  #333333;
-}
-.bold{
-  font-size: 45px;
-  font-weight: 700;
-  font-family: Montserrat;
-}
-.article-img{
-  grid-column: 8 / span 5;
-  grid-row: 7 / span 7;
-  font-family: Montserrat;
-  font-weight: 700;
-  color: #333333;
-  background-color: #BD9E94;
-}
-.article-text{
-  grid-column: 4 / span 4;
-  grid-row: 7 / span 7;
-  display: flex;
-  padding-top: 40px;
-  justify-content: center;
-  /*font-family: Montserrat;*/
-  font-size: 15px;
-  position: relative;
-  /*border: 1px solid black;*/
-}
-#line{
-  width: 20%;
-  height: 2px;
-  background-color: #333333;
-  position: absolute;
-  margin-top: 60px;
-  margin-right: 50%;
-}
-.article-paragraph{
-  position: absolute;
-  margin-top: 100px;
-  padding: 0 70px;
-  font-size: 20px
-}
-.article-img2{
-  grid-column: 8 / span 5;
-  grid-row: 15 / span 7;
-  background-color: #BD9E94;
-}
-.article-text2{
-  grid-column: 4 / span 4;
-  grid-row: 15 / span 7;
-  display: flex;
-  justify-content: center;
-  padding-top: 40px;
-  font-size: 14px;
-  position: relative;
-}
-.article-img3{
-  grid-column: 8 / span 5;
-  grid-row: 23 / span 7;
-  background-color: #BD9E94;
-}
-.article-text3{
-  grid-column: 4 / span 4;
-  grid-row: 23 / span 7;
-  display: flex;
-  justify-content: center;
-  padding-top: 40px;
-  font-size: 14px;
-  position: relative;
-}
-.article-img4{
-  grid-column: 8 / span 5;
-  grid-row: 31 / span 7;
-  background-color: #BD9E94;
-}
-.article-text4{
-  grid-column: 4 / span 4;
-  grid-row: 31 / span 7;
-  display: flex;
-  justify-content: center;
-  padding-top: 40px;
-  font-size: 14px;
-  position: relative;
-}
-/*----------FOOTER----------*/
-.footer{
-  grid-column: 4 / span 9;
-  grid-row: 47 / span 8;
-  background-image: url("https://images.unsplash.com/photo-1524671807765-9614d7622fea?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3257d8872c89efc8503e971900467d3d&auto=format&fit=crop&w=900&q=60");
-  background-attachment: fixed;
+.article2{
+  height: 100vh;
   background-color: aquamarine;
-  background-size: contain;
+  margin-left: 360px;
+}
+.article3{
+  height: 100vh;
+  background-color: beige;
+  margin-left: 360px;
 }
 </style>
